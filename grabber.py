27bd18e487b5aeaed02c151ee0e860e3e4e94173
Tom Bottiglieri <tom.bottiglieri@gmail.com>
2012-03-01 22:58:23 -0800

    Pymongo data store and better stuff

--- grabber.py
+++ grabber.py
@@ -1,10 +1,11 @@
 import twitter
 import json
+from matches import MatchStore
 
-def getMatchData(text) :
-  i = text.split()
+def get2012MatchData(status) :
+  i = status.text.split()
   match = {}
-  match['event'] = i[0]
+  match['event'] = i[0][4:]
   match['type'] = i[2]
   match['number'] = int(i[4])
   match['red_final'] = int(i[6])
@@ -22,10 +23,53 @@ def getMatchData(text) :
   match['red_teleop'] = int(i[30])
   match['blue_teleop'] = int(i[32])
   match['coopertition'] = int(i[34])
+  match['t_id'] = s.id
+  match['date'] = s.created_at
   return match 
 
-api = twitter.Api()
-statuses = api.GetUserTimeline("frcfms")
+def get2011MatchData(status) :
+  i = status.text.split()
+  match = {}
+  match['event'] = i[0][4:]
+  match['type'] = i[2]
+  match['number'] = int(i[4])
+  match['red_final'] = int(i[6])
+  match['blue_final'] = int(i[8])
+  red_teams = [ int(i[10]), int(i[11]), int(i[12]) ]
+  match['red_alliance'] = red_teams
+  blue_teams = [ int(i[14]), int(i[15]), int(i[16]) ]
+  match['blue_alliance'] = blue_teams
+  match['red_bonus'] = int(i[18])
+  match['blue_bonus'] = int(i[20])
+  match['red_fouls'] = int(i[22])
+  match['blue_fouls'] = int(i[24])
+  match['t_id'] = s.id
+  match['date'] = s.created_at
+  return match 
 
-for s in statuses : 
-  print  json.dumps(getMatchData(s.text))
+
+api = twitter.Api()
+
+
+numToFetch = 100
+running = True
+i=1
+curPage = 1
+ms = MatchStore('2012')
+lastIDFetched = ms.getLatestMatchID()
+
+while running :
+  statuses = api.GetUserTimeline("frcfms", since_id=lastIDFetched, count=numToFetch, page=curPage)
+  print "Fetched",len(statuses),"matches"
+  for s in statuses : 
+    m = get2012MatchData(s)
+    ms.addMatch(m)
+    print s.text
+    print i
+    i = i + 1
+
+  if len(statuses) < numToFetch :
+    running = False
+  else :
+    curPage += 1
+
---
+++ matches.py
@@ -0,0 +1,17 @@
+import pymongo
+
+class MatchStore:
+  firstID = 158306092244418561 # First meaningful tweet!
+
+  def __init__(self, year='2012'):
+    self.conn = pymongo.Connection()
+    db = 'fms' + year
+    self.matches = self.conn[db].matches
+
+  def getLatestMatchID(self):
+    for m in self.matches.find().sort("t_id", pymongo.DESCENDING).limit(1):
+      return m['t_id']
+    return self.firstID
+  
+  def addMatch(self, match):
+    self.matches.save(match)
